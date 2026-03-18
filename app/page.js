a6812cf2cce70d551d49ef78e679a91d3579203d
Lakshmitriveni25 <kalyankumargoparaju@Mac.lan>
2026-03-17 19:29:34 -0500

    add real UI

--- app/page.js
+++ app/page.js
@@ -12,7 +12,7 @@ export default function Quiz() {
 
   const handleSubmit = async () => {
     try {
-      const res = await fetch("http://127.0.0.1:8000/recommend", {
+      const res = await fetch("https://ai-skincare-backend.onrender.com/recommend", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
